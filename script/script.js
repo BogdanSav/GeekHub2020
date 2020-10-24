b82e262494ddf4d4638658ac0282f0421f23d698
BogdanSav <bohdan16@live.com>
2020-10-24 11:42:41 +0300

    homework #1

--- script/script.js
+++ script/script.js
@@ -5,11 +5,20 @@ document.querySelectorAll('input[name], #formula').forEach(function(input) {
         let a2 = document.querySelector('[name="a2"]');
         let b2 = document.querySelector('[name="b2"]');
         let formula = document.querySelector('#formula');
+        let condition = document.querySelector('#condition');
         let result = document.querySelector('#result');
-        let calc = (a1, b1, a2, b2) => {
-            return formula.value;
+        try {
+
+            let calc = new Function('a1,b1,a2,b2', 'return ' + formula.value + ';');
+            result.value = calc(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
+            let checkCondition = new Function('a1,b1,a2,b2',
+                'if(' + condition.value + '){ result.style.backgroundColor = "green"; } else result.style.backgroundColor ="white"');
+            checkCondition(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
+
+        } catch (error) {
+            result.value = "#ERROR";
+            // console.error(error.message);
         }
-        result.value = calc(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
     });
 
 });
