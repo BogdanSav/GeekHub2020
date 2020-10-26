aa4a57595159a2e18f5985642735d805fd904c00
BogdanSav <bohdan16@live.com>
2020-10-27 00:00:22 +0200

    homework #1.2

--- functions/index.js
+++ functions/index.js
@@ -10,11 +10,11 @@ document.querySelectorAll('input[name], #formula').forEach(function(input) {
         try {
 
             let calc = new Function('a1,b1,a2,b2', 'return ' + formula.value + ';');
-            result.value = calc(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
+
 
             let checkCondition = new Function('a1,b1,a2,b2', 'if(' + condition.value + '){ result.style.backgroundColor = "rgb(182, 215, 168)"; } else result.style.backgroundColor ="rgb(255,255,255)";');
-
             checkCondition(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
+            result.value = calc(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
 
         } catch (error) {
             result.value = "#ERROR";
