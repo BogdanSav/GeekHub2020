00a3e02502aaf4bd9aaa78c0e8169f1e8fb1cdee
BogdanSav <bohdan16@live.com>
2020-10-28 14:10:00 +0200

    homework #1.3

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,30 @@
-document.querySelectorAll('input[name], #formula').forEach(function(input) {
-    input.addEventListener('keyup', function() {
-        let a1 = document.querySelector('[name="a1"]');
-        let b1 = document.querySelector('[name="b1"]');
-        let a2 = document.querySelector('[name="a2"]');
-        let b2 = document.querySelector('[name="b2"]');
-        let formula = document.querySelector('#formula');
-        let condition = document.querySelector('#condition');
-        let result = document.querySelector('#result');
-        try {
+    "use strict"
+    document.querySelectorAll('input[name], #formula').forEach(function(input) {
+        input.addEventListener('keyup', function() {
+            let a1 = document.querySelector('[name="a1"]');
+            let b1 = document.querySelector('[name="b1"]');
+            let a2 = document.querySelector('[name="a2"]');
+            let b2 = document.querySelector('[name="b2"]');
+            let formula = document.querySelector('#formula');
+            let condition = document.querySelector('#condition');
+            let result = document.querySelector('#result');
 
-            let calc = new Function('a1,b1,a2,b2', 'return ' + formula.value + ';');
+            try {
 
 
-            let checkCondition = new Function('a1,b1,a2,b2', 'if(' + condition.value + '){ result.style.backgroundColor = "rgb(182, 215, 168)"; } else result.style.backgroundColor ="rgb(255,255,255)";');
-            checkCondition(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
-            result.value = calc(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
+                let calc = new Function('a1,b1,a2,b2', 'return ' + formula.value + ';');
+                let Condition = new Function('a1,b1,a2,b2', 'return ' + condition.value + ";");
 
-        } catch (error) {
-            result.value = "#ERROR";
-            // console.error(error.message);
-        }
+
+                result.value = calc(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
+                if (Condition(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value))) {
+                    result.style.backgroundColor = "rgb(182, 215, 168)";
+                } else result.style.backgroundColor = "rgb(255,255,255)";
+
+            } catch (error) {
+                result.value = "#ERROR";
+                console.error(error.message);
+            }
+        });
+
     });
-
-});
