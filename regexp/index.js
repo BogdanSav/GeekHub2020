45a2b2cbe3e74540052e6af8ba0e821b283e7898
BogdanSav <bohdan16@live.com>
2020-11-02 00:53:13 +0200

    homework#2.1

--- regexp/index.js
+++ regexp/index.js
@@ -1,15 +1,24 @@
 document.querySelector('#user-form').addEventListener('submit', function(e) {
     e.preventDefault();
-    let nameRule = /[a-їА-ї]+\s+[a-їА-ї]+\s+[a-їА-ї]+\s?/;
-    let emailRule = /^[^@][a-zA-Z0-9.-]+[^\.]@[^\.][a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+[^@]\s?/;
-    let passwordRule = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z0-9]{8,}/
+    let nameRule = /[a-їА-ї]+\s+[a-їА-ї]+\s+[a-їА-ї]+$/;
+    let emailRule = /^[^@][a-zA-Z0-9.-]+[^\.]@[^\.][a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+[^@]$/;
+    let passwordRule = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z0-9]{8,}/;
     let name = document.querySelector('[name="full_name"]');
     let email = document.querySelector('[name="email"]');
     let password = document.querySelector('[name="password"]');
+    // let rules = [/[a-їА-ї]+\s+[a-їА-ї]+\s+[a-їА-ї]+\s?/,
+    //     /^[^@][a-zA-Z0-9.-]+[^\.]@[^\.][a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+[^@]\s?/,
+    //     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z0-9]{8,}/
+    // ];
+    if (!nameRule.test(name.value)) {
+        name.style.backgroundColor = "#F9D0C4";
+    } else name.style.backgroundColor = "#C2E0C6";
+    if (!emailRule.test(email.value)) {
+        email.style.backgroundColor = "#F9D0C4";
+    } else email.style.backgroundColor = "#C2E0C6";
     if (!passwordRule.test(password.value)) {
-        password.style.backgroundColor = "red";
-    } else password.style.backgroundColor = "white";
-
+        password.style.backgroundColor = "#F9D0C4";
+    } else password.style.backgroundColor = "#C2E0C6";
 });
 
 document.querySelectorAll('[data-show]').forEach(function(button) {
