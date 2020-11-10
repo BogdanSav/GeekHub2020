fd0450a12569af7f32808c4651837c94f8863074
BogdanSav <bohdan16@live.com>
2020-11-10 23:51:13 +0200

    homework#3.1

--- dom/index.js
+++ dom/index.js
@@ -1,47 +1,57 @@
-jQuery('input').on('paste', function (e) {
-	e.preventDefault();
+jQuery('input').on('paste', function(e) {
+    e.preventDefault();
 
-	var text = e.originalEvent.clipboardData.getData('text/plain');
-	var input = e.currentTarget;
+    var text = e.originalEvent.clipboardData.getData('text/plain');
+    var input = e.currentTarget;
+    let data = text.split("\n");
+    let splitedData = [];
+
+    for (let i = 0; i < data.length; i++) {
+        splitedData.push(data[i].split(";"));
+
+    }
+    console.log(splitedData);
+
+
 
 
 });
 
 var currentColumn;
 
-jQuery('thead th').on('contextmenu', function (e) {
-	e.preventDefault();
+jQuery('thead th').on('contextmenu', function(e) {
+    e.preventDefault();
 
-	currentColumn = e.currentTarget;
+    currentColumn = e.currentTarget;
 
-	var menu = jQuery('#column-menu');
+    var menu = jQuery('#column-menu');
 
-	menu.addClass('d-block');
+    menu.addClass('d-block');
 
-	menu.css({
-		left: e.clientX,
-		top: e.clientY
-	});
+    menu.css({
+        left: e.clientX,
+        top: e.clientY
+    });
 });
 
-jQuery('#column-menu [data-action]').on('click', function (e) {
-	e.preventDefault();
+jQuery('#column-menu [data-action]').on('click', function(e) {
+    e.preventDefault();
 
-	var action = e.currentTarget.getAttribute('data-action');
+    var action = e.currentTarget.getAttribute('data-action');
 
-	switch (action) {
-		case 'add-left':
+    switch (action) {
+        case 'add-left':
 
-			break;
+            break;
 
-		case 'add-right':
+        case 'add-right':
 
-			break;
+            break;
 
-		case 'remove':
+        case 'remove':
 
-			break;
-	}
+            break;
+    }
 
-	jQuery('#column-menu').removeClass('d-block');
+    jQuery('#column-menu').removeClass('d-block');
 });
