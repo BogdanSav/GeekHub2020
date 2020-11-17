e3ecc4448246738a49ab6bce747574781f4c42a7
BogdanSav <bohdan16@live.com>
2020-11-18 00:39:09 +0200

    homework 4.1

--- prototype/index.js
+++ prototype/index.js
@@ -1,10 +1,76 @@
 function Csv() {
-    this.parse = function(string, separartor = "") {
-        if (string.includes(";")) {
-            console.log("work");
+    this.parse = function(string, separator) {
+        this.string = string;
+        this.separator = separator || "";
+
+        this.result = [];
+        this.separatorsCount = {
+            comma: 0,
+            dotCom: 0,
+            tabulation: 0
+        };
+        this.separatorValue = {
+            dotCom: ";",
+            comma: ",",
+            tabulation: "\t"
+        };
+        if (!this.separator) {
+            for (key in this.separatorValue) {
+                for (i of this.string) {
+                    if (i === this.separatorValue[key]) {
+                        this.separatorsCount[key]++;
+                    }
+                }
+            }
+            console.log(this.separatorsCount);
+            for (key in this.separatorsCount) {
+                if (this.separatorsCount[key] === Math.max.apply(null, Object.values(this.separatorsCount))) {
+                    console.log(this.separatorValue[key]);
+                    this.string.split('\n').forEach(element => {
+                        this.result.push(element.split(this.separatorValue[key]));
+
+                    });
+
+                }
+            }
+
+        } else {
+            this.string.split('\n').forEach(element => {
+                this.result.push(element.split(separator));
+
+            });
         }
+        console.log(this.result);
+
+
     }
+    this.generate = function(array, separator) {
+        this.array = array;
+        this.separator = separator || "";
+        this.result;
+        if (!this.separator) {
+            array.forEach(elem => {
+                this.result += elem.join(", ");
+            });
+        } else {
+            array.forEach(elem => {
+                this.result += elem.join(separator);
+            });
+        }
+        console.log(this.result);
+    }
+
 }
-let testString = "Євпак Віктор Миколайович;ФОП;1985";
+
+
+
+let testString = "Євпак Віктор, Миколайович;ФОП;1985\nБондаренко Анатолій Васильович;міський голова;1974\nМойсієнко Василь Миколайович;перший проректор;1965";
+let testArray = [
+    ['Євпак Віктор, Миколайович', 'ФОП', '1985'],
+    ['Бондаренко Анатолій Васильович', 'міський голова', '1974'],
+    ['Мойсієнко Василь Миколайович', 'перший проректор', '1965']
+];
 let convertor = new Csv();
+// console.log(convertor.parse(testString));
 convertor.parse(testString);
+convertor.generate(testArray);
