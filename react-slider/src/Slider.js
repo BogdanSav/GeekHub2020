3fa74d244b86adfafdd0fbd6a4c885a9d639e157
BogdanSav <bohdan16@live.com>
2021-01-03 01:35:39 +0200

    react-slider 1

--- react-slider/src/Slider.js
+++ react-slider/src/Slider.js
@@ -1,14 +1,96 @@
 import React from 'react';
 import styled from 'styled-components';
+import Draggable from 'react-draggable';
+
 
 export default class Slider extends React.PureComponent {
+	constructor(props) {
+		super(props)
+		this.barRef = React.createRef();
+		this.HandlerRef = React.createRef();
+		this.state = {
+			value: this.props.value,
+
+			x: 0,
+
+		}
+		this.width = 0,
+			this.dx = 0;
+		this.startX = 0;
+
+		this.firstX = 0;
+
+
+	}
+	componentDidMount() {
+		this.width = this.barRef.current.clientWidth;
+		this.dx = this.width / (this.props.max - this.props.min);
+		this.setState({ x: this.props.value * this.dx });
+
+	}
+
+
+	onDragStart = (e) => {
+		this.firstX = e.clientX;
+		this.startX = e.clientX;
+		console.log(this.state.x);
+		console.log(this.firstX);
+		// this.firstY = e.clientY;
+		this.onDrag(e);
+		document.body.addEventListener("mousemove", this.onDrag);
+		document.body.addEventListener("mouseup", this.onDragEnd);
+
+
+	};
+	onDrag = (e) => {
+		let x = this.startX + e.clientX - this.firstX;
+
+		if (x < 0) {
+			x = 0;
+		}
+		else if (x> this.width) {
+			x = this.width;
+		}
+		this.setState({
+			x: this.startX + e.clientX - this.firstX,
+
+		});
+		if (this.startX < x + this.dx) {
+			this.setState({ value: Math.round(x / this.dx)+this.props.min });
+		}
+		if (this.startX > x - this.dx) {
+			this.setState({ value: Math.round(x / this.dx)+this.props.min });
+		}
+
+	};
+	onDragEnd = () => {
+		this.startX = this.state.x;
+		// console.log(this.state.x);
+		document.body.removeEventListener("mousemove", this.onDrag);
+		document.body.removeEventListener("mouseup", this.onDragEnd);
+	}
+	valueChange = (e) => {
+		this.setState({
+			value: e.target.value,
+			x: this.state.value *this.dx,
+		});
+		console.log(this.state.value);
+		this.startX = this.state.x;
+
+	}
+
+
+
+
 	render() {
+
 		return (
 			<Root>
-				<input/>
+				<input value={this.state.value} onChange={this.valueChange}/>
 
-				<Bar>
-					<Handler/>
+				<Bar ref={this.barRef}>
+					<Handler x={this.state.x} onMouseDown={this.onDragStart} ref={this.HandlerRef} />
+
 				</Bar>
 			</Root>
 		);
@@ -16,7 +98,7 @@ export default class Slider extends React.PureComponent {
 }
 
 
-//region ====================== Styles ========================================
+//#region ====================== Styles ========================================
 
 const Root = styled.div`
     padding: 10px 0;
@@ -29,14 +111,18 @@ const Bar = styled.div`
 	margin-top: 10px;
 `;
 
-const Handler = styled.div`
+const Handler = styled.div.attrs(props => ({
+	style: {
+		left: props.x,
+	},
+}))`
     position: absolute;
 	height: 10px;
 	width: 10px;
 	border-radius: 5px;
 	background-color: red;
 	top: -4px;
-	left: 0;
+	left: 20px;
 `;
 
-//endregion
+//#endregion
